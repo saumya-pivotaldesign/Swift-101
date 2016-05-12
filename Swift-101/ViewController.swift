2ce8cfbee6b71d232f20b52fa95a47f3e71c26b4
saumya <saumya@pivotaldesign.biz>
2016-05-12 20:19:41 +0530

    SWift 101 : starting project

--- Swift-101/ViewController.swift
+++ Swift-101/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var tInput:UITextField?
+    @IBOutlet var webView:UIWebView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +21,13 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onGo(){
+        let text = tInput?.text
+        let url = NSURL(string: text!)
+        let req = NSURLRequest(URL: url!)
+        webView?.loadRequest(req)
     }
 
 
